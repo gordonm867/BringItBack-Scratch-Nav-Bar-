77d2aab1ac5a2f897565cfd35ef30037675394b9
ItchyCatIII <ItchyCatIII@users.noreply.github.com>
2017-07-06 21:10:33 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,9 +1,9 @@
-var links = document.getElementsByTagName("li");
+var links = document.getElementsByTagName("li"); // Gets all "li" elements, including the tips button
 for (var i = 0;i<links.length;i++) {
-  if (String(links[i].innerHTML).valueOf() === '<a href="/tips"><span>Tips</span></a>'.valueOf()) {
-    links[i].innerHTML = '<a href="/discuss"><span>Discuss</span></a>';
+  if (String(links[i].innerHTML).valueOf() === '<a href="/tips"><span>Tips</span></a>'.valueOf()) { // If the HTML of an li element matches the HTML of the li element for the tips button on the new nav bar...
+    links[i].innerHTML = '<a href="/discuss"><span>Discuss</span></a>'; // change that li element's HTML to the HTML for an li element of a button that leads to the discussion forums
   };
   if (String(links[i].innerHTML).valueOf() === '<a href="/tips">Tips</a>'.valueOf()) {
-    links[i].innerHTML = '<a href="/discuss">Discuss</a>'
+    links[i].innerHTML = '<a href="/discuss">Discuss</a>' // works for pages still with the old nav bar too
   }
 }
